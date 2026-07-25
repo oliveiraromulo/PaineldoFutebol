3db9293536deeaa829e510064eb561f21ba4e5bf
Romulo Oliveira <romulo.silva.11@hotmail.com>
2026-07-25 18:48:56 -0300

    adding new request_api function with retry

--- Docs/Airflow/dags/soccer_analytics/etls/extract_fixture_stats.py
+++ Docs/Airflow/dags/soccer_analytics/etls/extract_fixture_stats.py
@@ -3,10 +3,14 @@ import json
 #import argparse
 #import config
 
-import psycopg as pg # pyright: ignore[reportMissingImports]
+import psycopg as pg
 
-file = open('/opt/airflow/dags/soccer_analytics/config.json')
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
 #file = open('/home/romulo/Documents/Git/PaineldoFutebol/Serie B 2021/Codigos/config.json')
+file = open('/opt/airflow/dags/soccer_analytics/config.json')
+
 args = json.load(file)
 headers = {
     'x-rapidapi-host': args['x-rapidapi-host'],
@@ -15,25 +19,46 @@ headers = {
 
 url = "https://api-football-v1.p.rapidapi.com/v3/fixtures/statistics"
 
+def request_api(url, header, kwargs):
+    session = requests.Session()
+
+    retry = Retry(connect=3,
+                  status_forcelist=[429],
+                  allowed_methods=["GET"],
+                  backoff_factor=60)
+    
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+
+    response = session.get(url, headers=header, params=kwargs)
+    return response
+
+
 def extract_fixtures_stats(url, headers, querystring, fixture_id):
-    response = requests.get(url, headers=headers, params=querystring)
+
+    #response = requests.get(url, headers=headers, params=querystring)
+    response = request_api(url, headers, querystring)
     json_payload = json.loads(response.text)
+    
+    if json_payload['results'] != 0:
+        result_set = []
+        result_set.append(fixture_id)
 
-    result_set = []
-    result_set.append(fixture_id)
+        for part in json_payload['response']:
+            team_id = part['team']['id']
+            team_name = part['team']['name']
 
-    for part in json_payload['response']:
-        team_id = part['team']['id']
-        team_name = part['team']['name']
+            result_set.append(team_id)
+            result_set.append(team_name)
 
-        result_set.append(team_id)
-        result_set.append(team_name)
+            for stats in part['statistics']:
+                if stats['type'] not in ['goals_prevented']:
+                    result_set.append(stats['value'])
+        return result_set
 
-        for stats in part['statistics']:
-            if stats['type'] not in ['goals_prevented']:
-                result_set.append(stats['value'])
-            
-    return result_set
+    else:
+        print(f"Fixture ID: {fixture_id} - No statistics available for this fixture.")
 
 '''
     Query to extract all fixture_ids from the last round of the championship
@@ -46,6 +71,7 @@ query_fixtures = """
         from dimension.dim_rounds dim
         where 1=1
         --and CURRENT_TIMESTAMP between start_round_date and end_round_date
+        and start_round_date >= current_timestamp - interval '30' DAY
         and cast(fix.league_id as int) = dim.league_id
         and cast(fix.league_season as int) = dim.league_season
         and fix.league_round = dim.league_round
@@ -63,10 +89,10 @@ fixtures_ids = cur.fetchall()
 result_list = []
 for fixture in fixtures_ids:   
     querystring = {"fixture": fixture[0]}
-    
-    result_list.append(extract_fixtures_stats(url, headers, querystring, fixture[0]))
 
-#print(result_list)
+    returned_result = extract_fixtures_stats(url, headers, querystring, fixture[0])
+    if returned_result is not None:
+        result_list.append(returned_result)
 
 insert_query = """INSERT INTO usr_landing.fixture_stats 
 values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,
